0f608771730e918a095bae68970b004f671d547f
Son Roy Almerol <github@snry.me>
2026-01-13 23:53:44 -0500

    remove bundled.json on generate

--- third_party/opensubsonic/generate.go
+++ third_party/opensubsonic/generate.go
@@ -2,3 +2,4 @@ package opensubsonic
 
 //go:generate npx @redocly/cli bundle https://raw.githubusercontent.com/opensubsonic/open-subsonic-api/refs/heads/main/openapi/openapi.json --output bundled.json --ext json
 //go:generate oapi-codegen -config cfg.yaml ./bundled.json
+//go:generate rm ./bundled.json
